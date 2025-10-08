f912b86c5d47e3c278f7117d2bded3fe910a089f
Jeffy P Jerry <Jeffy P Jerry@LAPTOP-BNCIGNIM>
2025-10-08 17:42:39 +0530

    project committed

--- Automation/src/test/java/seleniumBasics/BrowserAndNavigationCommands.java
+++ Automation/src/test/java/seleniumBasics/BrowserAndNavigationCommands.java
@@ -4,15 +4,27 @@ public class BrowserAndNavigationCommands extends Base{
 	public void browserCommand() {
 		String title=driver.getTitle();
 		System.out.println(title);
-		String source=driver.getPageSource();
-		System.out.println(source);
+		//String source=driver.getPageSource();
+		//System.out.println(source);
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		String handleid=driver.getWindowHandle();
+		System.out.println(handleid);
+	}
+	public void navigationCommand() {
+		driver.navigate().to("https://www.amazon.in/gp/bestsellers/?ref_=nav_em_cs_bestsellers_0_1_1_2");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 BrowserAndNavigationCommands command=new BrowserAndNavigationCommands();
 command.initializeBrowser();
-command.browserCommand();
+//command.browserCommand();
+command.navigationCommand();
+
 
 	}
 
