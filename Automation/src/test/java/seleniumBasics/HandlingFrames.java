7d7d0e725a1237ccdfdc62baa7846d540b3e39f3
Jeffy P Jerry <Jeffy P Jerry@LAPTOP-BNCIGNIM>
2025-10-21 20:03:01 +0530

    committed

---
+++ Automation/src/test/java/seleniumBasics/HandlingFrames.java
@@ -0,0 +1,28 @@
+package seleniumBasics;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandlingFrames extends Base{
+	public void verifyframes() {
+		driver.navigate().to("https://demoqa.com/frames");	
+		List<WebElement>totalframe=driver.findElements(By.tagName("iframe"));
+		System.out.println(totalframe.size());
+		WebElement frame1=driver.findElement(By.id("frame1"));
+		driver.switchTo().frame(frame1);
+		WebElement frameheading=driver.findElement(By.id("sampleHeading"));
+		System.out.println(frameheading.getText());
+		driver.switchTo().defaultContent();
+	}
+
+	public static void main(String[] args) {
+		HandlingFrames frame=new HandlingFrames();
+		frame.initializeBrowser();
+		frame.verifyframes();
+		
+
+	}
+
+}
