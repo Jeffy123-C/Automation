4aabf226908173c2ec1b59e6e94bb70311331a40
Jeffy P Jerry <Jeffy P Jerry@LAPTOP-BNCIGNIM>
2025-10-23 12:15:46 +0530

    committed

--- Automation/src/test/java/seleniumBasics/HandlingActions.java
+++ Automation/src/test/java/seleniumBasics/HandlingActions.java
@@ -1,5 +1,9 @@
 package seleniumBasics;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -18,14 +22,35 @@ public class HandlingActions extends Base {
 		
 		
 	}
+	public void verifyDragAndDrop() {
+		driver.navigate().to("https://demoqa.com/droppable");
+		WebElement drag=driver.findElement(By.id("draggable"));
+		WebElement drop=driver.findElement(By.id("droppable"));
+		Actions act=new Actions(driver);
+		act.dragAndDrop(drag, drop).build().perform();
+		
+	}
+	public void verifyKeyboardAction() throws AWTException {
+		Robot robot=new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_T);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_T);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HandlingActions action=new HandlingActions();
 		action.initializeBrowser();
 		//action.verifyRightClick();
-		action.verifyMouseHover();
+		//Actions act=new Actions(driver);action.verifyMouseHover();
+//action.verifyDragAndDrop();	
+try {
+	action.verifyKeyboardAction();
+} catch (AWTException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
 
-	}
-
+}
 }
