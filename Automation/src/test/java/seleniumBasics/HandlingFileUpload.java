e1de7bf7b52ade7bc49720b7c670a59bd667b11f
Jeffy P Jerry <Jeffy P Jerry@LAPTOP-BNCIGNIM>
2025-10-28 12:14:38 +0530

    Committed

---
+++ Automation/src/test/java/seleniumBasics/HandlingFileUpload.java
@@ -0,0 +1,55 @@
+package seleniumBasics;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandlingFileUpload extends Base{
+	public void fileUploadUsingSendKeys() {
+		driver.navigate().to(" https://demo.guru99.com/test/upload");
+		WebElement choosefile=driver.findElement(By.id("uploadfile_0"));
+		choosefile.sendKeys("C:\\Users\\Jeffy P Jerry\\git\\Automation\\Automation\\src\\test\\resources\\exam.pdf");
+		WebElement checkbox=driver.findElement(By.id("terms"));
+		checkbox.click();
+		WebElement submitbutton=driver.findElement(By.id("submitbutton"));
+		submitbutton.click();
+	}
+	public void fileUploadUsingRobot() throws AWTException {
+		driver.navigate().to("https://www.ilovepdf.com/pdf_to_word");
+		WebElement selectpdf=driver.findElement(By.id("pickfiles"));
+		selectpdf.click();
+		StringSelection selection=new StringSelection("C:\\Users\\Jeffy P Jerry\\git\\Automation\\Automation\\src\\test\\resources\\exam.pdf");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);
+		Robot robot=new Robot();
+		robot.delay(2500);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HandlingFileUpload upload=new HandlingFileUpload();
+		upload.initializeBrowser();
+		//upload.fileUploadUsingSendKeys();
+		try {
+			upload.fileUploadUsingRobot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
